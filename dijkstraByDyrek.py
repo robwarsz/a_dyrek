764f11460ea7811239f9e1a14af50d40f739f8a4
robwarsz <robertwarszawski@gmail.com>
2022-03-01 19:34:17 +0100

    Initial commit

---
+++ dijkstraByDyrek.py
@@ -0,0 +1,30 @@
+import heapq   #kopiec binarny
+
+INF = 2000000000
+
+V, E = map(int, input().split())
+dist = [INF] * (V+1)
+
+adj = []
+
+for _ in range(V+1):
+    adj.append([])
+
+for _ in range(E):
+    a,b,w = map(int, input().split())
+    adj[a].append((b,w))    #tuple
+    adj[b].append((a,w))
+
+start = 1
+dist[start] = 0
+li = [ (0,start)]     #(dystans, numerWierzcholka)
+heapq.heapify([li])
+while li:
+    d,u = heapq.heappop(li)      # d,u -> dystans, numerWierzcholka
+    for x in adj[u]:
+        v,weight = x
+        if dist[v] > dist[u]+weight:
+            dist[v]=dist[u]+weight
+            heapq.heappush(li,dist[v],v)
+
+print(dist[1:])
